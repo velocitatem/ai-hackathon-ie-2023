2a1d4f034358a48a20a639e71257a8931b7f1297
Daniel Rosel <daniel@alves.world>
2023-11-20 22:27:05 +0100

    added atomization and other stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from clai import *
+import argparse
 import os
 # load .env
 from dotenv import load_dotenv
@@ -9,28 +10,30 @@ load_dotenv()
 # print(ct)
 
 
+def docall(directory, output_name, gpt4=False):
+    stack = []
+    targets = [fn for fn in os.listdir(directory) if fn.lower().endswith('.pdf')]
+    for filename in sorted(targets):
+        pdf_path = os.path.join(filename)
+        print("Getting ", pdf_path)
+        data = extract_data(pdf_path, gpt4)
+        stack.append(data.dict())
+        if data is not None:
+            betas_to_csv(stack, output_name)
 
-directory = "data_0611"
-stack = []
-for ff in get_all_files():
-    # chek if file exists
-    if os.path.exists(directory + "/" + ff):
-        print(ff)
-        try:
-            ct = extract_data(ff)
-            if ct is None:
-                continue
-            print(ct)
-            stack.append(ct.dict())
-            betas_to_csv(stack, "output.csv")
-        except Exception as e:
-            # throw the error
-            print(e)
-            continue
+    betas_to_csv(stack, output_name)
 
 
 
+def main():
+    parser = argparse.ArgumentParser(description='Extract structured product data from PDF documents.')
+    parser.add_argument('dataset', type=str, help='Path to the directory containing PDF documents.')
+    parser.add_argument('output_name', type=str, help='Name of the output CSV file. Ex: out.csv')
+    parser.add_argument('--gpt4', action='store_true', help='Use GPT-4 instead of GPT-3.5 Turbo')
 
-betas_to_csv(stack, "output.csv")
-# cs = betas_to_csv([ts.dict()])
-# print(cs)
+    args = parser.parse_args()
+
+    docall(args.dataset, args.output_name, args.gpt4)
+
+if __name__ == "__main__":
+    main()
