7915e4475050a92ea89c17d284b8d5c0b0e011d4
IsmaPi <ismael.picazo.flores@icloud.com>
2023-11-22 19:18:03 +0100

    changed csv_comparison

--- csv_comparison.py
+++ csv_comparison.py
@@ -1,34 +1,38 @@
 import pandas as pd
 import difflib
 
-def read_csv(file_path):
-    return pd.read_csv(file_path)
+def read_csv(file_path, columns_to_ignore):
+    data = pd.read_csv(file_path)
+    return data.drop(columns=columns_to_ignore, errors='ignore')
 
-def read_excel_as_csv(excel_path):
-    return pd.read_excel(excel_path, engine='openpyxl').to_csv()
+def read_excel_as_csv(excel_path, columns_to_ignore):
+    data = pd.read_excel(excel_path, engine='openpyxl')
+    return data.drop(columns=columns_to_ignore, errors='ignore').to_csv(index=False)
 
 def compare_files(file1, file2):
     differences = list(difflib.unified_diff(
-        file1.splitlines(), file2.splitlines(), 
+        file1.splitlines(), file2.splitlines(),
         lineterm='', fromfile='file1', tofile='file2'
     ))
     return differences
 
 def main():
-    csv_file_path = 'path_to_your_csv_file.csv'
-    excel_file_path = 'path_to_your_excel_file.xlsx'
+    csv_file_path = 'path_csv'
+    excel_file_path = 'path_xlsx'
+    columns_to_ignore = ['Issuer', 'Underlying(s)']
 
-    csv_content = read_csv(csv_file_path)
-    converted_excel_content = read_excel_as_csv(excel_file_path)
+    csv_content = read_csv(csv_file_path, columns_to_ignore)
+    converted_excel_content = read_excel_as_csv(excel_file_path, columns_to_ignore)
 
-    differences = compare_files(csv_content.to_csv(), converted_excel_content)
+    differences = compare_files(csv_content.to_csv(index=False), converted_excel_content)
 
-    if differences:
-        print("Differences found:")
-        for line in differences:
-            print(line)
+    diff_count = len(differences)
+    if diff_count > 0:
+        print(f"Differences found: {diff_count}")
     else:
         print("No differences found.")
 
+if __name__ == "__main__":
+    main()
 
 
